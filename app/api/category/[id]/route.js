b4fcb3143a19414371d55d6f680ab97e62585c90
Sirinalin <u6510240@au.edu>
2024-10-11 11:39:20 +0700

    fix2

--- app/api/category/[id]/route.js
+++ app/api/category/[id]/route.js
@@ -1,9 +1,21 @@
 import Category from "@/models/Category";
+import { connectToDB } from "@/utils/database";
 
 export async function GET(request, { params }) {
-    const id = params.id;
-    const category = await Category.findById(id)
-    return Response.json(category);
+    try {
+        await connectToDB();
+        const id = params.id;
+        const category = await Category.findById(id);
+        
+        if (!category) {
+            return Response.json({ error: "Category not found" }, { status: 404 });
+        }
+        
+        return Response.json(category);
+    } catch (error) {
+        console.error("Error fetching category:", error);
+        return Response.json({ error: "Internal Server Error" }, { status: 500 });
+    }
 }
 
 export async function DELETE(request, { params }) {
